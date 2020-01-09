adf181fe3d1f221ad22b9aba688ccf86514cf40b
Eugene Anischenko <43790223+anischenkozhenya@users.noreply.github.com>
2020-01-09 08:27:10 +0300

    Final version of the method "AddOrChangeUrlParameter"

--- TypeComparisonAndCloning/Task3/MyStaticClass.cs
+++ TypeComparisonAndCloning/Task3/MyStaticClass.cs
@@ -1,40 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Task3
 {
     public static class MyStaticClass
     {
+        /// <summary>
+        /// Method add or change Url with parameter
+        /// </summary>
+        /// <param name="url">Original Url</param>
+        /// <param name="keyValueParameter">New parameters</param>
+        /// <returns>New Url with new parametrs</returns>
         public static string AddOrChangeUrlParameter(string url, string keyValueParameter)
         {
             if (url == null || keyValueParameter == null)
             {
                 throw new ArgumentNullException();
             }
-            //url = Regex.Replace(url,keyValueParameter.Split('=')[0]+@"=[^&]*", keyValueParameter);
-            //return url + (url.IndexOf(keyValueParameter) >= 0 ? "": (url.IndexOf('?') >= 0 ?"&":"?") + keyValueParameter);
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
             string patternKeyValue = @"(?<key>[a-zA-Z0-9]*)=(?<value>[a-zA-Z0-9]*)";
             string patternLink = @"^([a-zA-Z0-9.:\/]*)+?";
-            string link = new Regex(patternLink).Match(url).Value;
+            StringBuilder link = new StringBuilder(new Regex(patternLink).Match(url).Value);
             var keyValueUrl = new Regex(patternKeyValue).Matches(url);
             var KeyValueParam = new Regex(patternKeyValue).Matches(keyValueParameter);
-            int n = keyValueUrl.Count;
             Dictionary<string, string> keyValuesUrlDictionary = new Dictionary<string, string>();
             foreach (Match item in keyValueUrl)
             {
-                keyValuesUrlDictionary.Add(item.Groups["key"].Value,item.Groups["value"].Value);
+                keyValuesUrlDictionary.Add(item.Groups["key"].Value, item.Groups["value"].Value);
             }
             Dictionary<string, string> keyValuesParamDictionary = new Dictionary<string, string>();
             foreach (Match item in KeyValueParam)
             {
                 keyValuesParamDictionary.Add(item.Groups["key"].Value, item.Groups["value"].Value);
             }
-            if (keyValuesParamDictionary.Count>0)
+            if (keyValuesParamDictionary.Count > 0)
             {
                 foreach (var i in keyValuesParamDictionary)
                 {
@@ -48,18 +48,17 @@ namespace Task3
                     }
                 }
             }
-            if (keyValuesUrlDictionary.Count>0)
+            if (keyValuesUrlDictionary.Count > 0)
             {
-                link+= "?";
+                link.Append("?");
                 foreach (var i in keyValuesUrlDictionary)
                 {
-                    link += i.Key + "=" + i.Value + "&";
+                    link.Append(i.Key + "=" + i.Value + "&");
                 }
-                link = link.Substring(0, link.Length - 1);
+                
+                link.Remove(link.Length-1,1);
             }
-            return link;
-
+            return link.ToString();
         }
     }
 }
-
