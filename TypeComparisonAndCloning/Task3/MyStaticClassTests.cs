40014f28e3edbd2fcca72642655ba69c88041697
Eugene Anischenko <43790223+anischenkozhenya@users.noreply.github.com>
2020-01-09 08:57:31 +0300

    added new tests and renamed whit correct format

--- TypeComparisonAndCloning/Task3/MyStaticClassTests.cs
+++ TypeComparisonAndCloning/Task3/MyStaticClassTests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 
 namespace Task3
 {
@@ -6,31 +6,34 @@ namespace Task3
     class MyStaticClassTests
     {
         [Test]
-        public void test1()
+        public void AddNewParameterToUrlWithoutParameter()
         {
             Assert.AreEqual("www.example.com?key=value", MyStaticClass.AddOrChangeUrlParameter("www.example.com", "key=value"));
             }
         [Test]
-        public void test2()
+        public void AddNewParameterToUrlWithParameter()
         {
             Assert.AreEqual("www.example.com?key=value&key2=value2", MyStaticClass.AddOrChangeUrlParameter("www.example.com?key=value", "key2=value2"));
-            }
-        [Test]
-        public void test3()
-        {
-            Assert.AreEqual("www.example.com?key=newValue1", MyStaticClass.AddOrChangeUrlParameter("www.example.com?key=oldValue", "key=newValue1"));
         }
         [Test]
-        public void test4()
+        public void ChangeOldValueInUrlOnNewValueFromParameterWithSameKey()
+        {
+            Assert.AreEqual("www.example.com?key=newValue", MyStaticClass.AddOrChangeUrlParameter("www.example.com?key=oldValue", "key=newValue"));
+            Assert.AreEqual("www.example.com?key=newValue&key2=oldValue2", MyStaticClass.AddOrChangeUrlParameter("www.example.com?key=oldValue&key2=oldValue2", "key=newValue"));
+            Assert.AreEqual("www.example.com?key=oldValue&key2=newValue2", MyStaticClass.AddOrChangeUrlParameter("www.example.com?key=oldValue&key2=oldValue2", "key2=newValue2"));
+            Assert.AreEqual("www.example.com?key=newValue&key2=newValue2", MyStaticClass.AddOrChangeUrlParameter("www.example.com?key=oldValue&key2=oldValue2", "key=newValue&key2=newValue2"));
+        }
+        [Test]
+        public void AddEmptyParameter()
         {
             Assert.AreEqual("www.example.com", MyStaticClass.AddOrChangeUrlParameter("www.example.com?", ""));
         }
         [Test]
-        public void test5()
+        public void NullArguments()
         {
             Assert.Throws<System.ArgumentNullException>(() => MyStaticClass.AddOrChangeUrlParameter("", null));
             Assert.Throws<System.ArgumentNullException>(() => MyStaticClass.AddOrChangeUrlParameter(null, ""));
             Assert.Throws<System.ArgumentNullException>(() => MyStaticClass.AddOrChangeUrlParameter(null, null));
-        }
+        }        
     }
 }
